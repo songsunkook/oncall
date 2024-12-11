6a108b8c5aa950868a5d6f4d8c215b51342beec6
songsunkook <songsunkook@gmail.com>
2024-12-11 23:00:09 +0900

    refactor: Month enum 추가

--- src/main/java/oncall/domain/company/Calendar.java
+++ src/main/java/oncall/domain/company/Calendar.java
@@ -1,13 +1,10 @@
 package oncall.domain.company;
 
-import java.util.List;
-
 import oncall.domain.day.Days;
+import oncall.domain.day.Month;
 import oncall.domain.work.Worker;
 
 public class Calendar {
-
-    private static final List<Integer> _31days = List.of(1, 3, 5, 7, 8, 10, 12);
 
     private Company company;
 
@@ -15,16 +12,12 @@ public class Calendar {
         this.company = company;
     }
 
-    public Schedule scheduleWith(int month, String dayOfTheWeek) {
-        return calculateSchedule(month, dayOfTheWeek, _31days.contains(month) ? 31 : 30);
-    }
-
-    private Schedule calculateSchedule(int month, String dayOfTheWeekParam, int lastDay) {
+    public Schedule scheduleWith(int month, String dayOfTheWeekParam) {
         Schedule schedule = new Schedule(month, dayOfTheWeekParam);
         Days dayOfWeek = Days.from(dayOfTheWeekParam);
 
         Worker beforeWorker = null;
-        for (int i = 1; i <= lastDay; i++) {
+        for (int i = 1; i <= Month.from(month).getLastDay(); i++) {
             Worker result = company.nextWorker(month, i, dayOfWeek, beforeWorker);
             schedule.add(i, dayOfWeek, result);
             beforeWorker = result;
